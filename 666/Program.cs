24f4e0715998d71aa76fd8fee50ac4e25f3e45b6
Q2key <dimatocky@gmail.com>
2016-05-11 00:31:26 +0400

    MSQL connection foo added

--- 666/Program.cs
+++ 666/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
+using System.Data.SqlClient;
 using System.Data.SQLite;
 using System.Linq;
 using System.Reflection;
@@ -12,7 +13,7 @@ namespace _666
 {
     internal class Program
     {
-        public enum Names
+        private enum Names
         {
             Dimka,
             Sashka,
@@ -33,7 +34,7 @@ namespace _666
             Satan,
             Solomonich
         }
-        public enum LastNames
+        private enum LastNames
         {
             Demonizer,
             Perturbator,
@@ -54,7 +55,7 @@ namespace _666
             FatBasterd,
             Geek
         }
-        public enum Weapons
+        private enum Weapons
         {
             Intellect,
             Boobs,
@@ -75,8 +76,88 @@ namespace _666
             AxeAndHammer,
             PowerOfProgramming
         }
-
         static void Main(string[] args)
+        {   
+            Mssqlconnect();           
+            Console.ReadKey(true);
+        }
+        private static void Mssqlconnect()
+        {
+            var con = new SqlConnection();
+            var cs = @"user id=username;" +
+                     @"password=password;server=VK\SQLEXPRESS;" +
+                     @"Trusted_Connection=yes;" +
+                     @"database=mydb; " +
+                     @"connection timeout=15";
+
+            con.ConnectionString = cs;
+            try
+            {
+                if (con.State != ConnectionState.Open)
+                {
+                    con.Open();
+                }
+                if (con.State == ConnectionState.Open)
+                {
+                    Console.WriteLine(@"connected");
+                    FillSqLdatabaseTable(con);
+                    SelectFromMssqlbase(con);
+                }
+            }
+            catch (SqlException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+        private static void FillSqLdatabaseTable(SqlConnection con)
+        {
+            using (var trans = con.BeginTransaction())
+            {
+                const string insertcmd = @"INSERT INTO guild (name, title, weapon) Values(@name,@title,@weapon);";
+                var rnd = new Random();
+                for (var i = 0; i < 100; i++)
+                {
+                    var cmd = new SqlCommand(insertcmd, con, trans);
+                    var name = (Names) rnd.Next(0, 18);
+                    var lastname = (LastNames) rnd.Next(0, 18);
+                    var weapons = (Weapons) rnd.Next(0, 18);
+                    cmd.Parameters.AddWithValue("@name", $@"{name}");
+                    cmd.Parameters.AddWithValue("@title", $@"{lastname}");
+                    cmd.Parameters.AddWithValue("@weapon", $@"{weapons}");
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (SqlException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                trans.Commit();
+                Console.WriteLine("Done");
+            }
+        }
+        private static void SelectFromMssqlbase(SqlConnection con)
+        {
+            const string selectcmd = @"SELECT name, title " +
+                                     @"FROM guild " +
+                                     @"WHERE name LIKE 'Satan';";
+            var cmd = new SqlCommand(selectcmd,con);
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(@"{0} {1}",reader["name"],reader["title"]);
+            }
+
+
+        }
+        private static void Sqliteconnect()
         {
             const string constring = @"Data Source =D:\db.db; Version = 3;";
             using (var con = new SQLiteConnection(constring))
@@ -102,12 +183,11 @@ namespace _666
                 using (var trans = con.BeginTransaction())
                 {
                     var rnd = new Random();
-                    
                     for (var i = 0; i < 30000; i++)
                     {
-                        var name = (Names) rnd.Next(0, 18);
-                        var lastname = (LastNames) rnd.Next(0, 18);
-                        var weapons = (Weapons) rnd.Next(0, 18);
+                        var name = (Names)rnd.Next(0, 18);
+                        var lastname = (LastNames)rnd.Next(0, 18);
+                        var weapons = (Weapons)rnd.Next(0, 18);
                         cmd.Parameters.AddWithValue("@Name", $@"{name}");
                         cmd.Parameters.AddWithValue("@Title", $@"{lastname}");
                         cmd.Parameters.AddWithValue("@Weapon", $@"{weapons}");
@@ -123,9 +203,7 @@ namespace _666
                     trans.Commit();
                     Console.WriteLine("Done");
                 }
-
             }
-            Console.ReadKey(true);
         }
     }
 }
