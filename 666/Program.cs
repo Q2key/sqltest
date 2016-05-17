68bcffd637826a80e563a7c9d7c0667e369227a0
Q2key <dimatocky@gmail.com>
2016-05-17 15:29:32 +0400

    updated

--- 666/Program.cs
+++ 666/Program.cs
@@ -57,28 +57,89 @@ namespace _666
         }
         private enum Weapons
         {
-            Intellect,
-            Boobs,
-            Beauty,
-            Chlen,
-            ChlenSashki,
-            PowerOfBiceps,
-            ZaryazhenniTAZ,
             Dagger,
-            Ak47,
-            Bazzoka,
-            Bfg,
-            M16,
-            AnalHole,
-            ShinyBalls,
-            LongLongLongLongLongNumber,
-            ShinySmile,
-            AxeAndHammer,
-            PowerOfProgramming
+            Knife,
+            DualDaggers,
+            Mace,
+            Blunt,
+            Polearm,
+            Pistol,
+            DualPistols,
+            Rifle,
+            DualSwords,
+            TwoHandedSwords,
+            Axe,
+            Poleaxe,
+            Katar,
+            ShortBow,
+            LongBow,
+            LaserRifle,
+            CrossBow,
+            Sai
         }
         static void Main(string[] args)
-        {   
-            Mssqlconnect();           
+        {
+
+
+            var msk = new Dictionary<string,Dictionary<string,int>>();
+            var spb = new Dictionary<string, Dictionary<string, int>>();
+            var ekb = new Dictionary<string, Dictionary<string, int>>();
+
+            var frommskto = new Dictionary<string,int>();
+            var fromspbto = new Dictionary<string, int>();
+            var fromekbto = new Dictionary<string, int>();
+
+            frommskto.Add(@"Saint Petersburg",350);
+            frommskto.Add(@"Ekaterinburg",810);
+
+            fromspbto.Add(@"Moscow",350);
+            fromspbto.Add(@"Ekaterinburg",1200);
+
+            fromekbto.Add(@"Moscow",810);
+            fromekbto.Add(@"Saint Petersburg",1200);
+            
+            msk.Add(@"Moscow",frommskto);
+            spb.Add(@"Saint Petersburg",fromspbto);
+            ekb.Add(@"Ekaterinburg",fromekbto);
+
+            var list = new List<Dictionary<string, Dictionary<string, int>>> {msk, spb, ekb};
+            var path = new Dictionary<string,int>();
+
+            foreach (var dict in list)
+            {   
+                foreach (var subdict in dict)
+                {
+                    foreach (var curdict in subdict.Value)
+                    {
+
+                    }
+                }
+            }
+
+
+            
+
+
+
+            /*
+            var list = new int[4][];
+
+            list[0] = moscow;
+            list[1] = spb;
+            list[2] = ekb;
+            list[3] = kazan;
+
+            var mydict = new Dictionary<string, int[]>
+            {
+                {@"Moscow", moscow},
+                {@"Saint Petersburg", spb},
+                {@"Ekaterinburg", ekb},
+                {@"Kazan", kazan}
+            };
+            mydict.ToList().ForEach(t=>Console.WriteLine(@"{0} {1}",t.Key, PrFoo(t.Value)));
+             */
+
+
             Console.ReadKey(true);
         }
         private static void Mssqlconnect()
@@ -101,7 +162,7 @@ namespace _666
                 {
                     Console.WriteLine(@"connected");
                     FillSqLdatabaseTable(con);
-                    SelectFromMssqlbase(con);
+                    FillSqLdatabaseTableTwo(con);
                 }
             }
             catch (SqlException exception)
@@ -112,20 +173,18 @@ namespace _666
             {
                 con.Close();
             }
-
         }
         private static void FillSqLdatabaseTable(SqlConnection con)
         {
             using (var trans = con.BeginTransaction())
             {
                 const string insertcmd = @"INSERT INTO guild (name, title, weapon) Values(@name,@title,@weapon);";
-                var rnd = new Random();
-                for (var i = 0; i < 100; i++)
+                for (var i = 0; i < 18; i++)
                 {
                     var cmd = new SqlCommand(insertcmd, con, trans);
-                    var name = (Names) rnd.Next(0, 18);
-                    var lastname = (LastNames) rnd.Next(0, 18);
-                    var weapons = (Weapons) rnd.Next(0, 18);
+                    var name = (Names) i;
+                    var lastname = (LastNames) i;
+                    var weapons = (Weapons) i;
                     cmd.Parameters.AddWithValue("@name", $@"{name}");
                     cmd.Parameters.AddWithValue("@title", $@"{lastname}");
                     cmd.Parameters.AddWithValue("@weapon", $@"{weapons}");
@@ -143,19 +202,41 @@ namespace _666
                 Console.WriteLine("Done");
             }
         }
+        private static void FillSqLdatabaseTableTwo(SqlConnection con)
+        {
+            using (var trans = con.BeginTransaction())
+            {
+                const string insertcmd = @"INSERT INTO guildmembers (name, age, strength, dexterity, intellect,vitality)" +
+                                         "values (@name, @age, @strength, @dexterity, @intellect, @vitality);";
+                var rnd = new Random();
+                for (var i = 0; i < 18; i++)
+                {
+                    var cmd = new SqlCommand(insertcmd, con, trans);
+                    var name = (Names)i;
+                    var age = rnd.Next(18, 40);
+ 
+                    cmd.Parameters.AddWithValue("@name", $@"{name}");
+                    cmd.Parameters.AddWithValue(@"age", $@"{age}");
+                    cmd.Parameters.AddWithValue(@"strength", $@"{rnd.Next(5, 20)}");
+                    cmd.Parameters.AddWithValue(@"dexterity", $@"{rnd.Next(5, 20)}");
+                    cmd.Parameters.AddWithValue(@"intellect", $@"{rnd.Next(5, 20)}");
+                    cmd.Parameters.AddWithValue(@"vitality", $@"{rnd.Next(5, 20)}");
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (SqlException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                trans.Commit();
+                Console.WriteLine("Done");
+            }
+        }
         private static void SelectFromMssqlbase(SqlConnection con)
         {
-            const string selectcmd = @"SELECT name, title " +
-                                     @"FROM guild " +
-                                     @"WHERE name LIKE 'Satan';";
-            var cmd = new SqlCommand(selectcmd,con);
-            var reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                Console.WriteLine(@"{0} {1}",reader["name"],reader["title"]);
-            }
-
-
         }
         private static void Sqliteconnect()
         {
@@ -204,6 +285,18 @@ namespace _666
                     Console.WriteLine("Done");
                 }
             }
+        }
+        private static int PrFoo(int[] array)
+        {
+            var min = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (min > array[i])
+                {
+                    min = array[i];
+                }
+            }
+            return min;
         }
     }
 }
